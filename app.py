17069c496bd09c371b9f913146b2d67f2d79cf54
gambika28 <153752580+gambika28@users.noreply.github.com>
2026-04-08 20:32:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=1000)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001"
+    )
+    vector_store = FAISS.from_texts(
+        text_chunks,
+        embedding=embeddings
+    )
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context. 
+    Make sure to provide all the details.
+    If the answer is not available in the provided context, just say:
+    "answer is not available in the context", don't provide the wrong answer.
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(
+        model="gemini-pro",
+        temperature=0.3
+    )
+
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["context", "question"]
+    )
+
+    chain = prompt | model
+
+    return chain
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    context = "\n".join([doc.page_content for doc in docs])
+    response = chain.invoke({
+        "context": context,
+        "question": user_question
+    })
+
+    print(response)
+    st.write("Reply: ", response.content_blocks) 
+
+def main():
+    st.set_page_config("Chat With Multiple PDF")
+    st.header("Chat with Multiple PDF using Gemini 💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button")
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+if __name__ =="__main__":
+    main()
